9757d7f6ae52f3ec832b3016fc30044d2d62389d
Christoph Rüßler <christoph.ruessler@mailbox.org>
2024-11-01 08:30:20 +0100

    Rename snapshot

--- src/main.rs
+++ src/main.rs
@@ -477,7 +477,7 @@ mod tests {
 
 		draw(&mut terminal, &app).unwrap();
 
-		assert_snapshot!(terminal.backend());
+		assert_snapshot!("app_loading", terminal.backend());
 
 		let event =
 			AsyncNotification::Git(AsyncGitNotification::Status);
